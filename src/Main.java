129401a4c27b4e95b1d9622bda8345cb25154441
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-17 13:28:12 +0200

    Add generator methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
   public static void main(String[] args) {
+  }
 
-    }
+  private static CoolingTower createCoolingTower(double height, double diameter) {
+    return new CoolingTower(height, diameter);
+  }
+
+  private static Generator createGenerator(double electricPower, int numberOfTurbineBlades) {
+    return new Generator(electricPower, numberOfTurbineBlades);
+  }
+
+  private static NuclearReactor createNuclearReactor(double heatOutput, boolean requiresHeavyWater) {
+    return new NuclearReactor(heatOutput, requiresHeavyWater);
+  }
+
+  private static NuclearPowerPlant createNuclearPowerPlant(String name, String description, CoolingTower coolingTower, Generator generator, NuclearReactor nuclearReactor) {
+    return new NuclearPowerPlant(name, description, coolingTower, generator, nuclearReactor);
+  }
+
 }
