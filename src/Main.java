ffdae039a087e66b9f0a2d682ddad0e27e3cc579
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-17 13:50:03 +0200

    Create objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String[] args) {
+    CoolingTower firstCoolingTower = createCoolingTower(80, 4);
+    CoolingTower secondCoolingTower = createCoolingTower(100, 8);
+    Generator firstGenerator = createGenerator(16000,12);
+    Generator secondGenerator = createGenerator(18000,14);
+    NuclearReactor firstNuclearReactor = createNuclearReactor(14000, true);
+    NuclearReactor secondNuclearReactor = createNuclearReactor(24000, false);
+    NuclearPowerPlant dukovany = createNuclearPowerPlant("Dukovany","Popis Dukovan...", firstCoolingTower, firstGenerator, firstNuclearReactor);
+    NuclearPowerPlant temelin = createNuclearPowerPlant("Temelín","Popis Temelínu...", secondCoolingTower, secondGenerator, secondNuclearReactor);
   }
 
   private static CoolingTower createCoolingTower(double height, double diameter) {
